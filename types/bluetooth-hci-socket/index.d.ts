e0ea421d2e002adbb8d5957964a589b36cae75cd
johnny <johnny@localmomentum.net>
2018-09-04 06:22:19 -0400

    [ts] add typescript, types, and tsconfig
    This configuration is not strict, but gives a nice starting point.
    
    Also, the types here should be published to DefinitelyTyped or included
    in their respective projects.

---
+++ types/bluetooth-hci-socket/index.d.ts
@@ -0,0 +1,33 @@
+import * as event from 'events';
+
+declare class BluetoothHciSocket extends event.EventEmitter {
+  _isUp: boolean;
+  _hciEventEndpointBuffer: Buffer;
+  _aclDataInEndpointBuffer: Buffer;
+  constructor();
+  setFilter(filter?: Buffer): void;
+  bindRaw(devId: number): void;
+  bindUser(devId: number): void;
+  bindControl(): void;
+  getDeviceList(): BluetoothHciSocket.HciSocketDeviceInfo[];
+  isDevUp(): boolean;
+  start(): void;
+  stop(): void;
+  write(data: Buffer): void;
+  onHciEventEndpointData(data: Buffer): void
+  onAclDataInEndpointData(data: Buffer): void
+  reset(): void;
+}
+
+declare namespace BluetoothHciSocket {
+  interface HciSocketDeviceInfo {
+    devId?: string;
+    devUp?: string;
+    idVendor?: string;
+    idProduct?: string;
+    busNumber?: number;
+    deviceAddress?: number;
+  }
+}
+
+export = BluetoothHciSocket;
